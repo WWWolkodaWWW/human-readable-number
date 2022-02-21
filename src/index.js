a68448d672f1a397a899452f22b9396df1b13938
WWWolkodaWWW <80260640+WWWolkodaWWW@users.noreply.github.com>
2022-02-21 14:46:16 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+	number = String(number);
+	let zeroToNine = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	let tenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	let twentyToNinety = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	let hundred = 'hundred';
+
+	for (i = 0; i < number.length; i++) {
+
+		if (number.length === 1) {
+			j = number;
+			return zeroToNine[j];
+		}
+
+		else if (number.length === 2) {
+			j = number[0];
+			e = number[1];
+
+			if (j === '1') {
+				return tenToNineteen[e];
+			}
+
+			else if (e === '0') { 
+				return twentyToNinety[j];
+			}
+
+			else { 
+				return twentyToNinety[j] + " " + zeroToNine[e];
+			}
+		}
+
+		else if (number.length === 3) {
+			j = Math.floor(number / 100);
+			e = number[1];
+			f = number[2];
+
+			if (e === '0' && f === '0') { 
+				return zeroToNine[j] + " " + hundred;
+			}
+			
+			else if (e === '0') {
+				return zeroToNine[j] + " " + hundred + " " + zeroToNine[f];
+			}
+
+			else if (e === '1') {
+				return zeroToNine[j] + " " + hundred + " " + tenToNineteen[f];
+			}
+
+			else if (f === '0') {
+				return zeroToNine[j] + " " + hundred + " " + twentyToNinety[e];
+			}
+
+			else { 
+				return zeroToNine[j] + " " + hundred + " " + twentyToNinety[e] + " " + zeroToNine[f];
+			}
+		}
+	}
 }
